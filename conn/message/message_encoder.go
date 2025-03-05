15009fe13d9546100637c0fc064784d9d3ca2f70
xingyu <xingyu@beeplay123.com>
2025-03-05 17:04:05 +0800

    修改编解码方式

--- conn/message/message_encoder.go
+++ conn/message/message_encoder.go
@@ -22,8 +22,6 @@ package message
 
 import (
 	"encoding/binary"
-
-	"github.com/topfreegames/pitaya/util/compression"
 )
 
 // Encoder interface
@@ -67,35 +65,35 @@ func (me *MessagesEncoder) Encode(message *Message) ([]byte, error) {
 	}
 
 	buf := make([]byte, 0)
-	flag := byte(message.Type) << 1
-
+	//flag := byte(message.Type) << 1
+	flag := byte(message.Type)
 	routesCodesMutex.RLock()
 	code, compressed := routes[message.Route]
 	routesCodesMutex.RUnlock()
-	if compressed {
-		flag |= msgRouteCompressMask
-	}
-
-	if message.Err {
-		flag |= errorMask
-	}
+	//if compressed {
+	//	flag |= msgRouteCompressMask
+	//}
+	//
+	//if message.Err {
+	//	flag |= errorMask
+	//}
 
 	buf = append(buf, flag)
 
-	if message.Type == Request || message.Type == Response {
-		n := message.ID
-		// variant length encode
-		for {
-			b := byte(n % 128)
-			n >>= 7
-			if n != 0 {
-				buf = append(buf, b+128)
-			} else {
-				buf = append(buf, b)
-				break
-			}
-		}
-	}
+	//if message.Type == Request || message.Type == Response {
+	//	n := message.ID
+	//	// variant length encode
+	//	for {
+	//		b := byte(n % 128)
+	//		n >>= 7
+	//		if n != 0 {
+	//			buf = append(buf, b+128)
+	//		} else {
+	//			buf = append(buf, b)
+	//			break
+	//		}
+	//	}
+	//}
 
 	if routable(message.Type) {
 		if compressed {
@@ -107,17 +105,17 @@ func (me *MessagesEncoder) Encode(message *Message) ([]byte, error) {
 		}
 	}
 
-	if me.DataCompression {
-		d, err := compression.DeflateData(message.Data)
-		if err != nil {
-			return nil, err
-		}
-
-		if len(d) < len(message.Data) {
-			message.Data = d
-			buf[0] |= gzipMask
-		}
-	}
+	//if me.DataCompression {
+	//	d, err := compression.DeflateData(message.Data)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//
+	//	if len(d) < len(message.Data) {
+	//		message.Data = d
+	//		buf[0] |= gzipMask
+	//	}
+	//}
 
 	buf = append(buf, message.Data...)
 	return buf, nil
@@ -137,32 +135,33 @@ func Decode(data []byte) (*Message, error) {
 	m := New()
 	flag := data[0]
 	offset := 1
-	m.Type = Type((flag >> 1) & msgTypeMask)
+	//m.Type = Type((flag >> 1) & msgTypeMask)
+	m.Type = Type(flag)
 
 	if invalidType(m.Type) {
 		return nil, ErrWrongMessageType
 	}
 
-	if m.Type == Request || m.Type == Response {
-		id := uint(0)
-		// little end byte order
-		// WARNING: must can be stored in 64 bits integer
-		// variant length encode
-		for i := offset; i < len(data); i++ {
-			b := data[i]
-			id += uint(b&0x7F) << uint(7*(i-offset))
-			if b < 128 {
-				offset = i + 1
-				break
-			}
-		}
-		m.ID = id
-	}
+	//if m.Type == Request || m.Type == Response {
+	//	id := uint(0)
+	//	// little end byte order
+	//	// WARNING: must can be stored in 64 bits integer
+	//	// variant length encode
+	//	for i := offset; i < len(data); i++ {
+	//		b := data[i]
+	//		id += uint(b&0x7F) << uint(7*(i-offset))
+	//		if b < 128 {
+	//			offset = i + 1
+	//			break
+	//		}
+	//	}
+	//	m.ID = id
+	//}
 
 	m.Err = flag&errorMask == errorMask
 
 	if routable(m.Type) {
-		if flag&msgRouteCompressMask == 1 {
+		if true /*flag&msgRouteCompressMask == 1*/ {
 			m.compressed = true
 			code := binary.BigEndian.Uint16(data[offset:(offset + 2)])
 			routesCodesMutex.RLock()
@@ -183,12 +182,12 @@ func Decode(data []byte) (*Message, error) {
 	}
 
 	m.Data = data[offset:]
-	var err error
-	if flag&gzipMask == gzipMask {
-		m.Data, err = compression.InflateData(m.Data)
-		if err != nil {
-			return nil, err
-		}
-	}
+	//var err error
+	//if flag&gzipMask == gzipMask {
+	//	m.Data, err = compression.InflateData(m.Data)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//}
 	return m, nil
 }
